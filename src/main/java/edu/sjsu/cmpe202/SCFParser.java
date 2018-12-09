a1d63697e728138a8986d23782fdb2ba5ea30889
Karthik Ramasamy <karthira@cisco.com>
2018-12-08 18:28:41 -0800

    Add SCF Header and Body Parsing logic

--- src/main/java/edu/sjsu/cmpe202/SCFParser.java
+++ src/main/java/edu/sjsu/cmpe202/SCFParser.java
@@ -3,11 +3,14 @@ package edu.sjsu.cmpe202;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.commons.codec.binary.Hex;
 
+import edu.sjsu.cmpe202.util.TLV;
 import edu.sjsu.cmpe202.util.TLVParser;
 import edu.sjsu.cmpe202.util.TLVParserException;
 
@@ -30,12 +33,54 @@ public class SCFParser {
 			try {
 				File tlvFile = new File(args[0]);
 				if (tlvFile.exists()) {
-					byte[] array = Files.readAllBytes(tlvFile.toPath());
-					Map<String, Object> tlvContent = TLVParser.parseTLV(array);
+					byte[] tlvDataArray = Files.readAllBytes(tlvFile.toPath());
 					
-					for (Entry<String, Object> entry : tlvContent.entrySet()) {
-						System.out.println(entry.getKey() + " : " + Hex.encodeHexString((byte[]) entry.getValue()));
+					
+					//Parse the header
+					
+					if (tlvDataArray.length < 315) {
+						System.err.println("Invalid TLV File.");
+					}
+					
+					byte[] headerBytes = Arrays.copyOfRange(tlvDataArray, 0, 315);
+					
+					List<TLV> tlvHeader = TLVParser.parseTLV(headerBytes);
+					
+					System.out.println("\n SCF Header:");
+					System.out.println(" ===========");
+					
+					for (TLV tlv : tlvHeader) {
+						if (tlv.getTag().equalsIgnoreCase("0x04") || tlv.getTag().equalsIgnoreCase("0x06") || tlv.getTag().equalsIgnoreCase("0x0E")) {
+							System.out.println(tlv.getTag() + " : " + new String((byte[]) tlv.getValue()));
+						} else {
+							System.out.println(tlv.getTag() + " : " + Hex.encodeHexString((byte[]) tlv.getValue()));
+						}
+					}
+
+					//Parse the body
+					
+					byte[] bodyBytes = Arrays.copyOfRange(tlvDataArray, 316, tlvDataArray.length);
+					
+
+					List<TLV> tlvBody = TLVParser.parseTLV(bodyBytes);
+
+					System.out.println("\n SCF Body:");
+					System.out.println(" =========");
+					
+					int i = 0;
+
+					for (TLV tlv : tlvBody) {
 						
+						if (tlv.getTag().equalsIgnoreCase("0x01")) {
+							System.out.println("\n Certificate: " + ++i);
+							System.out.println(" ---------------");
+						}
+						
+						if (tlv.getTag().equalsIgnoreCase("0x02") || tlv.getTag().equalsIgnoreCase("0x03") || tlv.getTag().equalsIgnoreCase("0x05")) {
+							System.out.println(tlv.getTag() + " : " + new String((byte[]) tlv.getValue()));
+						} else {
+							System.out.println(tlv.getTag() + " : " + Hex.encodeHexString((byte[]) tlv.getValue()));
+						}
 					}
 				}
 			} catch (IOException ex) {
@@ -46,9 +91,6 @@ public class SCFParser {
 			}
 		}
 		
-
-
-
 	}
 
 }
