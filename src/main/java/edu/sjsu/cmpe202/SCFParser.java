c643028dd8ce3c7d995803ff9358afd9c60f60da
Karthik Ramasamy <karthira@cisco.com>
2018-12-08 18:31:54 -0800

    Removed unsued imports

--- src/main/java/edu/sjsu/cmpe202/SCFParser.java
+++ src/main/java/edu/sjsu/cmpe202/SCFParser.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.commons.codec.binary.Hex;
 
